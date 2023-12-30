74372e38872703e0264340875f13c36b2f908f4e
ovoskresenskiy <7949894@ukr.net>
2023-12-30 16:36:19 +0100

    Reworked welcome text. Added list of supported commands

--- src/main/java/com/vix/circustelegramchat/bot/util/AnswerTextMaker.java
+++ src/main/java/com/vix/circustelegramchat/bot/util/AnswerTextMaker.java
@@ -1,20 +1,37 @@
 package com.vix.circustelegramchat.bot.util;
 
 import com.vix.circustelegramchat.bot.Constants;
+import com.vix.circustelegramchat.model.Performance;
 import com.vix.circustelegramchat.model.Ticket;
 import com.vix.circustelegramchat.model.Visitor;
-import com.vix.circustelegramchat.model.Performance;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 
 import java.time.LocalDate;
 
 @Component
+@RequiredArgsConstructor
 public class AnswerTextMaker implements Constants {
 
+    private final BotUtil botUtil;
+
     public String welcomeText(Visitor visitor) {
-        return visitor.getState().equals(STATE_REGISTERED)
+        String greetings = visitor.getState().equals(STATE_REGISTERED)
                 ? "Welcome " + visitor.getFullName()
                 : TEXT_WELCOME_UNREGISTERED;
+
+        StringBuilder supportedCommands = new StringBuilder(greetings);
+        supportedCommands.append("\n\nSupported commands: \n");
+
+        for (BotCommand botCommand : botUtil.getSupportedCommands()) {
+            supportedCommands.append(botCommand.getCommand())
+                    .append(" - ")
+                    .append(botCommand.getDescription())
+                    .append("\n");
+        }
+
+        return supportedCommands.toString();
     }
 
     public String phoneNumberEntered(String phoneNumber) {
