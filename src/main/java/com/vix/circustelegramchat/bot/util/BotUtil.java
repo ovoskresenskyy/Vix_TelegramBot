5fac2f6ed9a7f7bfb0ee1c75eef9f50813520a87
ovoskresenskiy <7949894@ukr.net>
2023-12-30 16:04:47 +0100

    Added util methods: - building of BotCommand - making list of upcoming performances

--- src/main/java/com/vix/circustelegramchat/bot/util/BotUtil.java
+++ src/main/java/com/vix/circustelegramchat/bot/util/BotUtil.java
@@ -1,10 +1,14 @@
 package com.vix.circustelegramchat.bot.util;
 
 import com.vix.circustelegramchat.bot.Constants;
+import com.vix.circustelegramchat.model.Visitor;
+import com.vix.circustelegramchat.service.PerformanceService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
@@ -13,9 +17,15 @@ import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Component
+@RequiredArgsConstructor
 public class BotUtil implements Constants {
+
+    private final PerformanceService performanceService;
+    private final AnswerTextMaker answerTextMaker;
+    private final KeyboardCreator keyboardCreator;
 
     public SendMessage initNewMessage(String chatId, String text) {
         return initNewMessage(chatId, text, Collections.emptyList());
@@ -68,6 +78,28 @@ public class BotUtil implements Constants {
 
     public boolean isNameInvalid(String name) {
         return !NAME_FORMAT.matcher(name).matches();
+    }
+
+    public BotCommand getCommand(String command, String description) {
+        return BotCommand.builder()
+                .command(command)
+                .description(description)
+                .build();
+    }
+
+
+
+    public List<SendMessage> showUpcomingPerformances(Visitor visitor) {
+        Optional<LocalDate> optionalPerformanceDate = performanceService.getUpcomingPerformanceDate();
+        if (optionalPerformanceDate.isPresent()) {
+            LocalDate performanceDate = optionalPerformanceDate.get();
+            String text = answerTextMaker.navigationButtonPressed(performanceDate);
+            List<List<InlineKeyboardButton>> keyboard = keyboardCreator.getPerformanceKeyboard(performanceDate);
+
+            return List.of(initNewMessage(visitor.getChatId(), text, keyboard));
+        } else {
+            return List.of(initNewMessage(visitor.getChatId(), TEXT_NO_UPCOMING_PERFORMANCES));
+        }
     }
 
 }
